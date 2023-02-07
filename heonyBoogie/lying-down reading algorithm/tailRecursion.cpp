123a3a65b9c671de494ab9a94ffcf5855f2a689d
heonyBoogie <98378185+heonyBoogie@users.noreply.github.com>
2023-02-07 16:43:33 +0900

    꼬리재귀 코드 업로드
     꼬리재귀가 단순 재귀 보다 약3배 정도 시간이 빨랐음

---
+++ heonyBoogie/lying-down reading algorithm/tailRecursion.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int foo(int n)// 일반 재귀
+{
+	if (n == 0) return 1;
+	return 2*foo(n-1);
+}
+
+int foo2(int n,int t) // tail 재귀
+{
+	if (t == 0) return n;
+	return foo2(2*n,t-1);
+}
+
+int main() 
+{
+	chrono::system_clock::time_point start = chrono::system_clock::now();
+	foo(4000);
+	chrono::duration<double>sec = chrono::system_clock::now() - start;
+	cout << sec.count() << endl;
+	
+	start = chrono::system_clock::now();
+	foo2(1, 4000);
+	sec = chrono::system_clock::now() - start;
+	cout << sec.count() << endl;
+
+	return 0;
+}
+
+
